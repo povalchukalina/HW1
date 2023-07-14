72c0870c10a1f0800418c329c4975f911b1e7797
Alina < >
2023-07-14 23:46:22 +0300

    changes in script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+//1
+/*let a=20;
+alert(a); */
+
+//2
+/*alert(`Год выпуска первого IPhone`);
+let b=2007;
+alert(b);*/
+
+//3
+/*let name=('Brendon Eich');
+alert(name);*/
+
+//4
+/*let a = 10;
+let b = 2;
+alert(`Сумма ${a + b}`);
+alert(`Разность ${a - b}`);
+alert(`Произведение ${a * b}`);
+alert(`Частное ${a / b}`);*/
+
+
+//4.1
+/*let a = 10;
+let b = 2;
+let sum = a + b;
+alert(sum)
+let difference = a - b;
+alert(difference);
+let multiplication = a * b;
+alert(multiplication);
+let division = a / b;
+alert(division);*/
+
+//5
+/*let a = 2;
+let result = a**5;
+alert(result);*/
+
+//6
+/*let a = 9;
+let b = 2;
+let result = 9 % 2;
+alert(result);*/
+
+//7
+/*let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);*/
+
+//8
+/*let age = prompt('Сколько вам лет?');
+alert(age);*/
+
+//9.0
+/*let user {
+    name: 'Alina',
+    age: '20',
+    isAdmin: true,
+}
+//9.1
+let user["city of residence"] = 'Moscow';
+//9.2
+let age = 21;
+//9.3
+delete user["city of residence"];
+//9.4
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+console.log([info]);
+alert(user);*/
+
+//10
+let userName = prompt('Введите Ваше имя');
+alert(`Привет, ${userName} !`);
